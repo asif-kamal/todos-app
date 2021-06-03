d46e6d51260d76dc93cfd055a21bb0d0b1d39bcf
asif-kamal <asif.kamal1489@gmail.com>
2021-06-03 18:18:14 -0400

    Removed fetch.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  namespace api do
-    namespace v1 do
+  namespace :api do
+    namespace :v1 do
       resources :todo_items
       resources :todo_lists
       end
@@ -9,4 +9,3 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
-fetch(`http://localhost:3000/api/v1/todo_lists`)
