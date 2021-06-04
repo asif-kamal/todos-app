231d49ae6e6272d035201b9f24b691aa64b640bc
asif-kamal <asif.kamal1489@gmail.com>
2021-06-04 16:20:46 -0400

    Added CRUD actions.

--- app/controllers/api/v1/todo_items_controller.rb
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::TodoItemsController < ApplicationController
+
+    def index
+        @todoItems = TodoItem.all
+        render json: @todoItems
+    end
+
+    def create 
+        @todoItem = TodoItem.create(todoItem_params)
+        render json: @todoItem
+    end
+
+   
+
+    def destroy
+        @todoItem = TodoItem.find(params[:todo_list_id])
+        @todoItem.destroy
+    end
+
+    private
+
+    def todosItem_params
+        params.require(:todosItem).permit(:date, :task, :todo_item_number)
+    end
 end
