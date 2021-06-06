1d00d82f529e2a83e30eb4e55fe1748bef589dfd
asif-kamal <asif.kamal1489@gmail.com>
2021-06-06 16:06:41 -0400

    Added CRUD actions to TodoListsController.

--- app/controllers/api/v1/todo_lists_controller.rb
+++ app/controllers/api/v1/todo_lists_controller.rb
@@ -1,2 +1,27 @@
 class Api::V1::TodoListsController < ApplicationController
+
+    def index
+        @todoLists = TodoLists.all
+        render json: @todoLists
+    end
+
+    def create 
+        @todoList = TodoList.create(todoItem_params)
+        render json: @todoList
+    end
+
+   
+
+    def destroy
+        @todoList = TodoList.find(params[:id])
+        @todoList.destroy
+    end
+
+    private
+
+    def todoList_params
+        params.require(:todoList).permit(:name)
+    end
+end
+
 end
