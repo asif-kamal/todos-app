7b80761a65c67d60f380eb5c58ebfb4afb28cd62
asif-kamal <asif.kamal1489@gmail.com>
2021-06-08 10:34:42 -0400

    Nested todo_items routes under todo_lists.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :todo_items
-      resources :todo_lists
+      resources :todo_lists do
+        resources :todo_items
+      end
       end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
